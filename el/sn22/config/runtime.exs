327e1a3a46c54e768a47f104dc0f7b3a85d991b4
cranb3rry <chimery@gmail.com>
2022-07-31 01:40:11 +0300

    📍🖍

--- el/sn22/config/runtime.exs
+++ el/sn22/config/runtime.exs
@@ -34,7 +34,7 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "tym.pw"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :sn22, Sn22Web.Endpoint,
