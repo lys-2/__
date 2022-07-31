bacf43725b9cd8cd67071a20963c3055c1926686
cranb3rry <chimery@gmail.com>
2022-08-01 00:17:04 +0300

    📍🖍

--- el/sn22/assets/js/app.js
+++ el/sn22/assets/js/app.js
@@ -42,3 +42,20 @@ liveSocket.connect()
 // >> liveSocket.enableLatencySim(1000)  // enabled for duration of browser session
 // >> liveSocket.disableLatencySim()
 window.liveSocket = liveSocket
+
+let socket = new Socket("/socket", {params: {userToken: "123"}})
+socket.connect()
+
+let channel = socket.channel("room:123", {token: roomToken})
+channel.on("new_msg", msg => console.log("Got message", msg) )
+$input.onEnter( e => {
+  channel.push("new_msg", {body: e.target.val}, 10000)
+    .receive("ok", (msg) => console.log("created message", msg) )
+    .receive("error", (reasons) => console.log("create failed", reasons) )
+    .receive("timeout", () => console.log("Networking issue...") )
+})
+
+channel.join()
+  .receive("ok", ({messages}) => console.log("catching up", messages) )
+  .receive("error", ({reason}) => console.log("failed join", reason) )
+  .receive("timeout", () => console.log("Networking issue. Still waiting..."))
