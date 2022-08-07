0fb4a9cad03a5c9e0105f9ed8dfed97f48222d68
cranb3rry <chimery@gmail.com>
2022-08-08 00:18:29 +0300

    _

--- el/sn22/lib/sn22.ex
+++ el/sn22/lib/sn22.ex
@@ -26,7 +26,7 @@ defmodule Sn22 do
     Process.put(:rc, rc) # request count
 
     {:ok, s} = :gen_tcp.connect('irc.chat.twitch.tv', 6667, [:binary])
-    tt = ["/home/sn/sx/tw1", "/home/sn22/sx/tw1"]
+    tt = ["/home/sn/sx/tw", "/home/sn22/sx/tw"]
     for e <- tt do
       case File.read(e) do
       {:ok, t} -> :gen_tcp.send(s, "PASS " <>  t)
