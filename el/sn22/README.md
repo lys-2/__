74e353f523733d082eb525578f90173b7a73b29a
cranb3rry <chimery@gmail.com>
2022-08-13 23:23:20 +0300

    _

--- el/sn22/README.md
+++ el/sn22/README.md
@@ -1,6 +1,6 @@
 
 sed -i 's/bullseye/sid/' /etc/apt/sources.list;
-yes | apt update && yes | apt full-upgrade;
+yes | apt update && yes | apt full-upgrade; 
 yes | apt install tmux elixir erlang inotify-tools wrk certbot docker postgresql cmake git;
 git clone https://github.com/lys-2/aa;
 cd aa/el/sn22/;
@@ -15,3 +15,5 @@ mix local.rebar --force;
 
 certbot certonly;
 git pull; yes | mix local.hex && mix deps.get; mix assets.deploy; MIX_ENV=prod iex --name ? -S mix phx.server;
+
+//////////////////////////
--- el/sn22/lib/sn22_web/views/v1.ex
+++ el/sn22/lib/sn22_web/views/v1.ex
@@ -4,13 +4,55 @@ defmodule Sn22Web.V1 do
 
   def render(assigns) do
     ~H"""
-    aaa <%= @aaa %>
+    <button phx-click="inc_temperature" phx-window-keyup="update_temp">+</button>
+    aaa <%= @aaa;  inspect((:h3.k_ring_distances 577375545977733119,
+     1), limit: 9999) %>
+     <p><%= @aaa; cs2 = :h3.k_ring(577094071001022463,
+     1);
+     for c <- cs2, do: {:h3.get_base_cell(c), :h3.k_ring(c, 1)}
+     |> inspect
+     %></p>
+
+
+     <script>
+     document.addEventListener("keydown", keyDownHandler, false);
+      document.addEventListener("keyup", keyUpHandler, false);
+      function keyDownHandler(e) {
+        console.log(e);
+        if (e.key === "e" || e.key === "ArrowRight") {
+          document.getElementById("e").style.color = "blue";
+        } else if (e.key === "q" || e.key === "ArrowLeft") {
+          document.getElementById("q").style.color = "blue";
+        }
+      else if (e.key === "w" || e.key === "ArrowLeft") {
+        document.getElementById("w").style.color = "blue";
+      }
+    else if (e.key === "r" || e.key === "ArrowLeft") {
+      document.getElementById("r").style.color = "blue";
+    }
+      }
+
+
+
+      function keyUpHandler(e) {
+        if (e.key === "e" || e.key === "ArrowRight") {
+          document.getElementById("e").style.color = "green";
+        } else if (e.key === "q" || e.key === "ArrowLeft") {
+          document.getElementById("q").style.color = "green";
+        }
+      else if (e.key === "w" || e.key === "ArrowLeft") {
+        document.getElementById("w").style.color = "green";
+      }
+    else if (e.key === "r" || e.key === "ArrowLeft") {
+      document.getElementById("r").style.color = "green";
+    }
+      }
+     </script>
     """
   end
 
   def mount(_params, _, socket) do
     if connected?(socket), do: Process.send_after(self(), :update, 1000)
-    IO.inspect 1;
 
     put_flash(socket, :info, "It worked!");
     {:ok, assign(socket, :aaa, M2.get)};
@@ -22,5 +64,18 @@ defmodule Sn22Web.V1 do
     IO.inspect 1;
     {:noreply, assign(socket, :aaa, inspect M2.get2 M2.get)}
   end
+
+  def handle_event("inc_temperature", _value, socket) do
+    IO.puts 123123123;
+    {:noreply, assign(socket, %{})}
+  end
+
+  def handle_event("update_temp", %{"key" => "ArrowUp"}, socket) do
+    IO.puts 222;
+    {:noreply, assign(socket, %{})}
+
+
+  end
+
 
 end
