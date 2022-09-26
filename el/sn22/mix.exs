72819e4cdf50d1e36d8f8cac88a702986c169ab5
cranb3rry <chimery@gmail.com>
2022-09-26 22:22:01 +0300

    _

--- el/sn22/mix.exs
+++ el/sn22/mix.exs
@@ -39,7 +39,7 @@ defmodule Sn22.MixProject do
       {:phoenix_live_view, "~> 0.17.5"},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.6"},
-      {:esbuild, " >= 0.13.5", runtime: Mix.env() == :dev},
+      {:esbuild, " ~> 0.5.0", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
